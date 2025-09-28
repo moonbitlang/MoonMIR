d964396cceb21a39829a13726f6b1110dabf7cb8
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2025-09-28 19:44:45 +0800

    fix some problems

--- test.rkt
+++ test.rkt
@@ -2,7 +2,7 @@
 #lang racket
 
 ;; List of tests to run
-(define tests '(simple_ret simple_add many_args if while while2 order_args for array struct heap_arr ptr bin_int))
+(define tests '(simple_ret simple_add many_args if while while2 order_args for array struct heap_arr ptr bin_int linked_list))
 
 ;; Executes a command and captures its standard output by redirecting to a temporary file.
 (define (run-and-get-output cmd)
