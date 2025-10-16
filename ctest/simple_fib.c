9ecb1fabb154ea7a5ec73c279eaa7fcf567e7da2
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2025-10-16 18:42:57 +0800

    fix some problems

--- ctest/simple_fib.c
+++ ctest/simple_fib.c
@@ -1,3 +1,5 @@
+void print_int(int n);
+
 int fib(int n) {
     if (n <= 1) return n;
     int a = fib(n - 1);
@@ -6,5 +8,7 @@ int fib(int n) {
 }
 
 int main() {
-    return fib(10);
+    int result = fib(10);
+    print_int(result);
+    return 0;
 }
