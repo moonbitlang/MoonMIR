4003c150342b7913cef63aa98cf4030020683fec
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2025-10-27 19:59:34 +0800

    modify one

--- ctest/branch.c
+++ ctest/branch.c
@@ -23,6 +23,17 @@ int foo(int a, int b) {
   }
   return res;
 }
+
+int foo2(int a) {
+  switch (a) {
+    case 1: return 100;
+    case 2: return 200;
+    case 3: return 300;
+    case 4: return 400;
+    default: return -100;
+  }
+}
+
 int main () {
   int res = max(3, 5);
   printf("%d ", res);
@@ -30,6 +41,14 @@ int main () {
   int x = foo(150, 30);
   int y = foo(250, 10);
   printf("%d ", x + y);
+
+  int r1 = foo2(1);
+  int r2 = foo2(2);
+  int r3 = foo2(3);
+  int r4 = foo2(4);
+
+  int r = r1 + r2 + r3 + r4;
+  printf("%d\n", r);
 
   return 0;
 }
